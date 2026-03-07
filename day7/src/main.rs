cf820988ea65be35a7eb0e2d5cac05a36aaf0b3b
Benjamin Thomas <benjaminthomas1999@outlook.com>
2026-03-07 20:04:03 +0200

    Initial file parsing into enums

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,96 +1,74 @@
 use std::fs::{self};
 
-#[derive(Clone, Copy, Debug)]
-enum Operator {
-    Multiply,
-    Add,
+#[derive(Debug, Clone, Copy)]
+enum Value {
+    Empty,
+    Splitter,
+    Beam,
+    BeamEnd,
+    Start,
 }
 
-#[derive(Debug)]
-struct Expression {
-    terms: Vec<u64>,
-    operator: Operator,
-}
+fn parseInput () -> Vec<Vec<Value>> {
 
-fn read_file(file_path: &str) -> Option<Vec<Expression>> {//I need to change this to a result type
-    match fs::read_to_string(file_path) {
-        Ok(raw_input) => Some(make_expressions(&raw_input.lines().collect())),
-        Err(_e) => None,
-    }
-}
-
-fn make_expressions(lines : &Vec<&str>) -> Vec<Expression> {//I should change this to a result type
-                                                            //instead of panicking 
-    let operator_line = lines[lines.len()-1];
+    let input = fs::read_to_string("input")
+        .unwrap_or("!".to_string());
     
-    let mut output : Vec<Expression> = vec![];
-    let mut number_cache : Vec<u64> = vec![];
+    let input = ".......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............";
 
-    //work from right to left in the file in columns.
-    let mut column_cursor : i32 = (operator_line.len() -1) as i32;
-    while column_cursor >= 0 {
-        let mut num : String = "".to_string();
-        for line_number in 0..lines.len()-1 {
-            num.push(lines[line_number].chars().nth(column_cursor as usize).unwrap());
-        }
-        let num = num.trim().parse::<u64>().unwrap(); 
-        number_cache.push(num);
+    let mut output = vec![];
+    for line in input.lines() {
+        let mut line_cache = vec![];
+        for char in line.chars() {
+            match char {
+                '.' => line_cache.push(Value::Empty),
+                '^' => line_cache.push(Value::Splitter),
+                'S' => line_cache.push(Value::Start),
+                _ => panic!("Error while reading the file!"),
 
-        match operator_line.chars().nth(column_cursor as usize).unwrap() {
-            '*' => {
-                output.push(Expression {
-                    terms : number_cache.clone(),
-                    operator : Operator::Multiply
-                });
-                column_cursor -= 2;
-                number_cache.clear();
-            },
-            '+' => {
-                output.push(Expression {
-                    terms : number_cache.clone(),
-                    operator : Operator::Add
-                });
-                column_cursor -= 2;
-                number_cache.clear();
-            },
-            ' ' => {
-                column_cursor -= 1;
-            },
-            _ => panic!("Unknown character found on operator line at column: {column_cursor}"),
+            }
         }
+        output.push(line_cache);
     }
     output
 }
 
-fn evaluate_expression(expression: Expression) -> u64 {
-    match expression.operator {
-        Operator::Multiply => {
-            let mut result = 1;
-            for term in expression.terms {
-                result *= term;
-            }
-            result
+fn printBoard(board : &Vec<Vec<Value>>){
+    for line in board {
+        let mut line_string = "".to_string();
+        for value in line {
+            line_string.push(match value {
+                Value::Empty => '.',
+                Value::Beam => '|',
+                Value::Start => 'S',
+                Value::BeamEnd => '!',
+                Value::Splitter => '^',
+            });
         }
-        Operator::Add => {
-            let mut result = 0;
-            for term in expression.terms {
-                result += term;
-            }
-            result
-        }
+        println!("{line_string}");
     }
 }
 
 fn main() {
-    let expressions: Vec<Expression> = match read_file("input") {//this should be a result type
-        Some(file_input) => file_input,
-        None => panic!("Could not read and process file!"),
-    };
-    
-    let mut sum_of_expressions = 0;
-    for expression in expressions {
-        sum_of_expressions += evaluate_expression(expression);
+    let mut gameLoop = true;
+    let board = parseInput();
+    while gameLoop {
+        printBoard(&board);
+        gameLoop = false;
     }
-
-    println!("{sum_of_expressions}");
 }
